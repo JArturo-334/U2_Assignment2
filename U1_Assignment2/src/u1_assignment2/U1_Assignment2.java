758cda2e13eca330f4ee67253e13b5823e6846ac
PVega17102 <paul.vega.is@unipolidgo.edu.mx>
2023-02-07 09:53:12 -0600

    Ya jala

--- U1_Assignment2/src/u1_assignment2/U1_Assignment2.java
+++ U1_Assignment2/src/u1_assignment2/U1_Assignment2.java
@@ -1,16 +1,15 @@
 package u1_assignment2;
 
 import java.sql.*;
-import java.util.Scanner;
 import java.util.concurrent.locks.*;
 import java.util.logging.*;
 import javax.swing.*;
 
 public class U1_Assignment2 {
     String bd = "server";
-    String url = "jdbc:mysql://localhost:3306/server";
-    String username = "root";
-    String password = "";
+    String url = "jdbc:mysql://25.71.84.174/server";
+    String username = "artur";
+    String password = "123";
     String driver = "com.mysql.cj.jdbc.Driver";
     Connection cx = null;
     Statement stmt = null;
@@ -40,7 +39,7 @@ public class U1_Assignment2 {
                 stmt.executeUpdate("DROP DATABASE IF EXISTS server");
                 String sql = "CREATE DATABASE server";
                 stmt.executeUpdate(sql);
-                url = "jdbc:mysql://localhost:3306/server";
+                url = "jdbc:mysql://25.71.84.174/server";
                 cx=DriverManager.getConnection(url, username, password);
             
                 stmt = cx.createStatement();
@@ -52,11 +51,17 @@ public class U1_Assignment2 {
                 stmt.executeUpdate(sql2); 
            
                 // Insert data into the database
-                stmt.executeUpdate("INSERT INTO prueba (name, age) VALUES ('John', 30)");
+                stmt.executeUpdate("INSERT INTO prueba (name, age) VALUES ('Juan', 20)");
+                stmt.executeUpdate("INSERT INTO prueba (name, age) VALUES ('Carlos', 20)");
+                stmt.executeUpdate("INSERT INTO prueba (name, age) VALUES ('Jacky', 20)");
+                stmt.executeUpdate("INSERT INTO prueba (name, age) VALUES ('Cesar', 20)");
+                stmt.executeUpdate("INSERT INTO prueba (name, age) VALUES ('Alonso', 20)");
+                stmt.executeUpdate("INSERT INTO prueba (name, age) VALUES ('Arturo', 20)");
+                stmt.executeUpdate("INSERT INTO prueba (name, age) VALUES ('Paul', 20)");
                 
                 String[] options = {"Consult", "Update"};
                 int op = JOptionPane.showOptionDialog(null, "What you want to do:", "Select one!", 0, 3, null, options, options[0]);
-                if (op == 0){
+                if (op == 0){ //si selecciona consult
                     Thread Reader = new Thread(new Reader());
                     Reader.start();
                 }
@@ -73,13 +78,12 @@ public class U1_Assignment2 {
     }
     
     class Reader implements Runnable{
-
         public void run(){
             try{
                 lock.writeLock().lock();
                 cx=DriverManager.getConnection(url, username, password);
                 stmt = cx.createStatement();
-                String selectSql = "SELECT name, age, id FROM prueba";
+                String selectSql = "SELECT * FROM prueba";
                 stmt = cx.prepareStatement(selectSql);
                 ResultSet rs = stmt.executeQuery(selectSql);
                 while (rs.next()) {
@@ -97,32 +101,44 @@ public class U1_Assignment2 {
             }
         }
     }
+    
     class Writter implements Runnable{
         public void run(){
-        
             PreparedStatement stmt2 = null;
+            JTextField id = new JTextField();
+            JTextField name = new JTextField();
+            JTextField age = new JTextField();
+            Object[] message = {
+                "ID to update:", id,
+                "New name:", name,
+                "New age:", age
+            };
             try {
                 lock.writeLock().lock();
                 cx=DriverManager.getConnection(url, username, password);
                 stmt = cx.createStatement();
-                Scanner input = new Scanner(System.in);
-                
-                //  UPDATE DE DATOS
-                System.out.print("Update name: ");
-                String firstValue = input.nextLine();
-                System.out.print("Update age: ");
-                int secondValue = input.nextInt();
-                System.out.print("Update id: ");
-                int thirdValue = input.nextInt();
-                stmt2 = cx.prepareStatement("UPDATE prueba SET name = ?, age = ?, id = ?");
-                stmt2.setString(1, firstValue);
-                stmt2.setInt(2, secondValue);
-                stmt2.setInt(3, thirdValue);
+                int option = JOptionPane.showConfirmDialog(null, message, "Update", JOptionPane.OK_CANCEL_OPTION);
+                if (option == JOptionPane.OK_OPTION) {
+                    // Update
+                    String firstValue = name.getText();
+                    int secondValue = Integer.parseInt(age.getText());
+                    int thirdValue = Integer.parseInt(id.getText());
+                    stmt2 = cx.prepareStatement("UPDATE prueba SET name = ?, age = ? WHERE id = ?");
+                    stmt2.setString(1, firstValue);
+                    stmt2.setInt(2, secondValue);
+                    stmt2.setInt(3, thirdValue);
 
-                // EJECUTAR STATEMENT
-                int rowsAffected = stmt2.executeUpdate();
-                System.out.println("Number of rows updated: " + rowsAffected);
-                System.out.println("Data inserted successfully");
+                    // Execute statement
+                    int rowsAffected = stmt2.executeUpdate();
+                    System.out.println("Number of rows updated: " + rowsAffected);
+                    System.out.println("Data inserted successfully");
+                    
+                    // Create consult query
+                    Thread Reader = new Thread(new Reader());
+                    Reader.start();
+                } else {
+                    System.out.println("Proccess canceled");
+                }
             } catch (SQLException ex) {
                 Logger.getLogger(U1_Assignment2.class.getName()).log(Level.SEVERE, null, ex);
             } finally{
